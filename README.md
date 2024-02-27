541c9cd8ca9daa1604b53187a5199f010c48dcca
ebricenho <ebridesign@gmail.com>
2024-02-27 11:50:10 -0300

    Project-5

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Project-5
+# Descripción del Proyecto
+
+Este proyecto es una aplicación web desarrollada con Streamlit que permite explorar un conjunto de datos de anuncios de venta de coches en Estados Unidos. Proporciona funcionalidades para visualizar datos y realizar análisis exploratorio de forma interactiva.
+
+## Funcionalidades
+
+- Construir un histograma para visualizar la distribución de la variable "odometer" (kilometraje).
+- Construir un gráfico de dispersión para explorar la relación entre el año del modelo y el precio de venta.
+- Interactuar con los gráficos utilizando Streamlit para ajustar el zoom y la visualización.
--- app.py
+++ app.py
@@ -3,11 +3,12 @@ import pandas as pd
 import plotly_express as px
 
 car_data = pd.read_csv('D:/Project 5/Project-5/vehicles_us.csv') # leer los datos
-hist_button = st.button('Construir histograma') # crear un botón
+hist_button = st.button('Histograma Interactivo') # crear un botón
+scatter_button = st.button('Gráfico Interactivo')
 
 if hist_button: # al hacer clic en el botón
     # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    st.write('Crea un histograma para el conjunto de datos de anuncios de venta de coches')
 
 # crear un histograma
     fig = px.histogram(car_data, x="odometer")
@@ -15,14 +16,13 @@ if hist_button: # al hacer clic en el botón
             # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
 
-if scatter_button:  # Si se hace clic en el botón de gráfico de dispersión
+if scatter_button: # Si se hace clic en el botón de gráfico de dispersión
     # Escribir un mensaje
-    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    st.write('Crea un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
     
     # Crear un gráfico de dispersión
-    fig = px.scatter(car_data, x="year", y="price")
+    fig = px.scatter(car_data, x="model_year", y="price")
     
     # Mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
 
-    
